d7aa26f21b73d97b34a6b6dfb2730f9cec1102dc
rsssss12 <104445931+rsssss12@users.noreply.github.com>
2023-02-21 09:29:18 +0100

    feat: CoverageTest2.java

---
+++ src/test/java/com/thealgorithms/CoverageTest2.java
@@ -0,0 +1,22 @@
+package com.thealgorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.thealgorithms.datastructures.trees.BinaryTree;
+import com.thealgorithms.others.CRCAlgorithm;
+
+public class CoverageTest {
+    public static void main(String[] args) {
+        CRCAlgorithm c = new CRCAlgorithm("1001", 10, 0.1);
+
+        c.generateRandomMess();
+        c.divideMessageWithP(true);
+        c.divideMessageWithP(false);
+        c.divideMessageWithP(true);
+        c.changeMess();
+
+        System.out.println("\n===== CRCAlgorithm coverage test =====\n");
+        c.getCoverage();
+    }
+}
